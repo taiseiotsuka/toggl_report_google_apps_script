6b49ed44f12b7d1febae05516cedfcfdacf8e64e
taiseiotsuka <gifthelp2@gmail.com>
2020-04-03 01:40:57 +0900

    code refactoring

--- index.js
+++ index.js
@@ -1,31 +1,26 @@
+// --- User Settings
 const EMAIL = '***@***';
 const WORKSPACE_ID = 123456;
 const TOGGL_API_KEY = '*****';
-const SHEET_NAME = 'スプレッドシートのシート名(例:シート1)';
-
-const date = new Date();
-date.setDate(date.getDate() - 1);
-const yesterday = Utilities.formatDate(date, 'Asia/Tokyo', 'yyyy-MM-dd');
-
-const getTogglReport = () => {
-  const params = {
-    method:'get',
-    headers: {'Authorization' : 'Basic ' + Utilities.base64Encode(TOGGL_API_KEY + ':api_token')},
-    contentType: 'application/json',
-  };
-  return JSON.parse(UrlFetchApp.fetch(
-    'https://toggl.com/reports/api/v2/details?since=' + yesterday + '&until=' + yesterday + '&workspace_id=' + WORKSPACE_ID + '&user_agent=' + EMAIL,
-    params
-  ).getContentText()).data;
-}
-
+const SHEET_NAME = 'Sheet name of spreadsheet (e.g. Sheet 1)';
+// ---
 const writeTogglReport = () => {
-  const res = getTogglReport();
+  const date = new Date();
+  date.setDate(date.getDate() - 1);
+  const yesterday = Utilities.formatDate(date, 'Asia/Tokyo', 'yyyy-MM-dd');
+  const togglReport = JSON.parse(UrlFetchApp.fetch(
+    'https://toggl.com/reports/api/v2/details?since=' + yesterday + '&until=' + yesterday + '&workspace_id=' + WORKSPACE_ID + '&user_agent=' + EMAIL,
+    {
+      method:'get',
+      headers: {'Authorization' : 'Basic ' + Utilities.base64Encode(TOGGL_API_KEY + ':api_token')},
+      contentType: 'application/json',
+    }
+  ).getContentText()).data;
   const sht = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET_NAME);
   const lastRow = sht.getRange('A:A').getValues().filter(String).length;
   let nextRow;
   const color = 'Turquoise';
-  res.map((row, index) => {
+  togglReport.map((row, index) => {
     nextRow = lastRow + index + 1;
     sht.getRange('A' + nextRow).setBackground(color).setValue(yesterday);
     sht.getRange('C' + nextRow).setBackground(color).setValue(row.client);
