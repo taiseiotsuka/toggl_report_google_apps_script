a4a73cb211d91d50cc54c19f9628ee55df4b759d
taiseiotsuka <gifthelp2@gmail.com>
2020-04-03 10:38:34 +0900

    fix set description

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ const writeTogglReport = () => {
   ).getContentText()).data;
   const sht = SpreadsheetApp.getActiveSpreadsheet().getSheetByName(SHEET_NAME);
   const lastRow = sht.getRange('A:A').getValues().filter(String).length;
+  Logger.log('lastRow:' + lastRow);
   let nextRow;
   const color = 'Turquoise';
   togglReport.map((row, index) => {
@@ -26,6 +27,6 @@ const writeTogglReport = () => {
     sht.getRange('C' + nextRow).setBackground(color).setValue(row.client);
     sht.getRange('D' + nextRow).setBackground(color).setValue(row.tags[0]);
     sht.getRange('E' + nextRow).setBackground(color).setValue(Math.round((row.dur / 1000 / 60 / 60) * 10) / 10);
-    sht.getRange('F' + nextRow).setBackground(color).setValue(row.description);
+    sht.getRange('F' + nextRow).setBackground(color).setValue((row.description || row.project));
   });
 }
